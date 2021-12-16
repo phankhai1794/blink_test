b74831d08c23419ec2ac988f7defbccfaf74abf1
Dang Tran <minhdangcntt@gmail.com>
2021-12-16 03:50:19 +0000

    remove code

--- src/app/main/apps/export/components/index.js
+++
@@ -1,5 +0,0 @@
-export * from './TabPanel';
-export * from './ColorTabs';
-export * from './Stepper';
-export {default as BookingForm} from './Form/BookingForm';
-export {default as BookingFooter} from './BookingFooter';
