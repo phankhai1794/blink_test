4dbff35b8dba4e7d42c860d1d4ce86886f90bf9e
dongphan <dongphanit@gmail.com>
2022-05-13 14:10:08 +0700

    format time

--- src/app/main/transactions/Transaction.js
+++ src/app/main/transactions/Transaction.js
@@ -7,6 +7,19 @@ import { List } from '@material-ui/core';
 import { cyan } from '@material-ui/core/colors';
 import { format } from 'date-fns';
 import * as TransActions from '../apps/workspace/admin/store/actions/transaction';
+const DATE_OPTIONS_FULL = {
+  year: 'numeric',
+  month: 'short',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric'
+};
+const DATE_OPTIONS_WITHOUT_YEAR = {
+  month: 'short',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric'
+};
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -28,13 +41,13 @@ function Transaction(props) {
   function selectedChange(index) {
     setSelectedTab(index);
   }
+  const aaaa = false;
 
   useEffect(() => {
     if (myBL) {
       dispatch(TransActions.getBlTrans(myBL.id));
     }
   }, [myBL]);
-
   return (
     <div className="flex" style={{ marginTop: 70, width: 450, backgroundColor: 'white' }}>
       <div className="flex flex-col" style={{ width: '100%' }}>
@@ -59,9 +72,14 @@ function Transaction(props) {
                   <Typography
                     variant="h5"
                     style={{ fontWeight: selectedTab === index ? 'bold' : '400' }}>
-                    {format(new Date(), 'MM/dd/yyyy kk:mm')}
+                    {new Date(data.createdAt).toLocaleString(
+                      undefined,
+                      new Date().getFullYear() == new Date(data.createdAt).getFullYear()
+                        ? DATE_OPTIONS_WITHOUT_YEAR
+                        : DATE_OPTIONS_FULL
+                    )}
                   </Typography>
-                  {index == 0 ? <label style={{ fontSize: 12 }}>Phiên bản hiện tại</label> : null}
+                  {index == 0 ? <label style={{ fontSize: 12 }}>Current verions</label> : null}
                   <div className="flex flex-row" style={{ alignItems: 'center' }}>
                     <Avatar style={{ background: cyan[400], width: 10, height: 10, padding: 2 }}>
                       {user.displayName.charAt(0).toUpperCase()}
