403f897b09ed325fe5e8066dca3962a3c1f66c7c
RedNam <tech@rednam.dev>
2022-08-09 16:55:24 +0700

    mod: check err.response before parse

--- src/@shared/handleError.js
+++ src/@shared/handleError.js
@@ -2,7 +2,11 @@ import * as Actions from 'app/store/actions';
 
 export const handleError = (dispatch, err) => {
   console.error(err);
-  let { status, message } = err.response.data.error;
+  let status, message;
+  if ( err.response ) {
+    status = err.response.data.error.status;
+    message = err.response.data.error.message;
+  }
   if (!message) message = err.message || "Your token has expired";
   if (status === 401) {
     dispatch(Actions.checkAuthToken(false));
