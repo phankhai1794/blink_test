3069604da56ce85437ffbbf3c68fb727d74f0e79
namhuy291297 <nguyenvuongnamhuy@gmail.com>
2022-08-12 18:31:19 +0700

    update code

--- src/app/main/apps/workspace/components/InquiryEditor.js
+++ src/app/main/apps/workspace/components/InquiryEditor.js
@@ -1,7 +1,7 @@
 import { FuseChipSelect } from '@fuse';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getKeyByValue, displayTime, stateResquest, toFindDuplicates } from '@shared';
+import { getLabelById, displayTime, stateResquest, toFindDuplicates } from '@shared';
 import {
   FormControl,
   FormControlLabel,
@@ -64,7 +64,7 @@ const useStyles = makeStyles((theme) => ({
     '& .MuiFormGroup-root': {
       flexDirection: 'row'
     },
-    
+
   },
   button: {
     margin: theme.spacing(1),
@@ -438,7 +438,7 @@ const InquiryEditor = (props) => {
       <div className="flex justify-between" style={{ padding: '0.5rem', marginRight: '-15px' }}>
         <div style={{ fontSize: '22px', fontWeight: 'bold', color: '#BD0F72' }}>
           {currentEditInq.field
-            ? getKeyByValue(metadata['field'], currentEditInq.field)
+            ? getLabelById(metadata['field_options'], currentEditInq.field)
             : 'New Inquiry'}
         </div>
 
@@ -475,7 +475,7 @@ const InquiryEditor = (props) => {
                   src="/assets/images/icons/trash-gray.svg"
                 />
               </IconButton>
-            
+
             </Tooltip>
           )}
         </FormControl>
