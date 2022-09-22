8e7ccd8f8b64e8a3638e845458f2d7d59aa50a93
Nguyễn Ngọc Bình <binh.nn@cyberlogitec.com>
2022-09-22 10:27:24 +0000

    FIX #2uhdf4b show hide-all view all

--- src/app/main/apps/workspace/components/InquiryViewer.js
+++ src/app/main/apps/workspace/components/InquiryViewer.js
@@ -724,15 +724,8 @@ const InquiryViewer = (props) => {
 
   useEffect(() => {
     const el = document.getElementById(question.id);
-    setShowViewAll(false);
-    if (el) {
-      const oldClassName = el.className;
-      el.className = el.className.replace('-hideText', '')
-      if (el.getBoundingClientRect().height > 110) {
-        el.className = oldClassName;
-        setShowViewAll(true);
-      }
-    }
+    setShowViewAll(false); 
+    if (el && el.scrollHeight > el.clientHeight) setShowViewAll(true);
   }, [isLoadedComment]);
 
   const renderBtnReply = () => {
