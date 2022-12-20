750f4337040c57c26111a4d147bcbc7050ab9679
Minh Nguyen <minhnt28101998@gmail.com>
2022-12-20 07:32:58 +0000

    fix email UI

--- src/app/main/apps/workspace/store/actions/mail.js
+++ src/app/main/apps/workspace/store/actions/mail.js
@@ -17,9 +17,10 @@ export const SUGGEST_MAIL_ERROR = 'SUGGEST_MAIL_ERROR';
 export const SUGGEST_MAIL_SUCCESS = 'SUGGEST_MAIL_SUCCESS';
 
 export const INPUT_MAIL = 'INPUT_MAIL';
-export const SET_TAGS = 'SET_TAGS'
+export const SET_TAGS = 'SET_TAGS';
 
-export const SENDMAIL_SET_FORM = 'SENDMAIL_SET_FORM'
+export const SET_CC = 'SET_CC';
+export const SENDMAIL_SET_FORM = 'SENDMAIL_SET_FORM';
 
 
 export const sendMail =
@@ -29,7 +30,7 @@ export const sendMail =
       const inquiriesPendingProcess = inquiries.filter(op => op.process === 'pending');
       const inquiriesDraftProcess = inquiries.filter(op => op.process === 'draft');
       const listComment = await axios.all(inquiriesPendingProcess.map(q => loadComment(q.id)));
-      listComment.map((comment, index) => comment.length && replyInqs.push(inquiries[ index ].id));
+      listComment.map((comment, index) => comment.length && replyInqs.push(inquiries[index].id));
       dispatch({ type: SENDMAIL_LOADING });
       sendmail(myblId, from, toCustomer, toCustomerCc, toCustomerBcc, toOnshore, toOnshoreCc, toOnshoreBcc, subject, content, replyInqs, user)
         .then((res) => {
@@ -42,14 +43,14 @@ export const sendMail =
             else {
               blinkStsCd = "IN";
             }
-            
+
             if (toCustomer) {
               rtrnDesc = "Return to Customer via workspace";
             }
             else {
               rtrnDesc = "Return to Onshore via workspace";
             }
-            
+
             dispatch(AppActions.updateOpusStatus(bkgNo, blinkStsCd, rtrnDesc));
             dispatch(InquiryActions.checkSend(false));
             dispatch(Actions.toggleReload());
@@ -96,6 +97,12 @@ export function inputMail(state) {
   };
 }
 
+export function setCc(state) {
+  return {
+    type: SET_CC,
+    state: state
+  };
+}
 
 export function setTags(state) {
   return {
@@ -106,13 +113,13 @@ export function setTags(state) {
 
 export const autoSendMail = (mybl, inquiries, inqCustomer, inqOnshore, metadata, content, form) => async (dispatch) => {
   const getField = (keyword) => {
-    return metadata.field?.[ keyword ] || '';
+    return metadata.field?.[keyword] || '';
   };
 
   const getValueField = (content, keyword) => {
-    return content[ getField(keyword) ] || ''
+    return content[getField(keyword)] || ''
   };
-  const cloneInquiries = [ ...inquiries ];
+  const cloneInquiries = [...inquiries];
   cloneInquiries.forEach(q => {
     if (q.state === 'OPEN') q.state = 'INQ_SENT'; // inquiry
     else if (q.state === 'REP_Q_DRF') q.state = 'REP_Q_SENT'; // inquiry
@@ -123,8 +130,8 @@ export const autoSendMail = (mybl, inquiries, inqCustomer, inqOnshore, metadata,
   let contentOns = ''
   let subjectCus = ''
   let contentCus = ''
-  const hasCustomer = inquiries.some(inq => inq.receiver[ 0 ] === 'customer')
-  const hasOnshore = inquiries.some(inq => inq.receiver[ 0 ] === 'onshore')
+  const hasCustomer = inquiries.some(inq => inq.receiver[0] === 'customer')
+  const hasOnshore = inquiries.some(inq => inq.receiver[0] === 'onshore')
   const vvd = getValueField(content, VVD_CODE)
   const pod = getValueField(content, POD_CODE)
   const del = getValueField(content, DEL_CODE)
@@ -133,7 +140,7 @@ export const autoSendMail = (mybl, inquiries, inqCustomer, inqOnshore, metadata,
 
   if (hasOnshore && form.toOnshore && inqOnshore.length > 0) {
     const formOnshore = { ...form };
-    formOnshore[ 'toCustomer' ] = '';
+    formOnshore['toCustomer'] = '';
     subjectOns = `[Onshore - BL Query]_[${inqOnshore.join(', ')}] ${bkgNo}: VVD(${vvd}) + POD(${pod}) + DEL(${del})`
     contentOns = `Dear Onshore, \n\nWe need your assistance for BL completion.\nPending issue: [${inqOnshore.join(', ')}]`
     dispatch(sendMail({ myblId: mybl.id, bkgNo, ...formOnshore, subject: subjectOns, content: contentOns, inquiries: inquiries }));
@@ -141,7 +148,7 @@ export const autoSendMail = (mybl, inquiries, inqCustomer, inqOnshore, metadata,
 
   if (hasCustomer && form.toCustomer && inqCustomer.length > 0) {
     const formCustomer = { ...form };
-    formCustomer[ 'toOnshore' ] = '';
+    formCustomer['toOnshore'] = '';
     subjectCus = `[Customer BL Query]_[${inqCustomer.join(', ')}] ${bkgNo}: VVD(${vvd}) + POD(${pod}) + DEL(${del})`
     contentCus = `Dear Customer, \n\nWe found discrepancy between SI and OPUS booking details or missing/ incomplete information on some BL's fields as follows: [${inqCustomer.join(', ')}]`
     dispatch(sendMail({ myblId: mybl.id, bkgNo, ...formCustomer, subject: subjectCus, content: contentCus, inquiries: inquiries }));
