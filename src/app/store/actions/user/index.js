6d3bc7eb20598588b5fd224ba96a8dfd5386e84c
namhuy291297 <nguyenvuongnamhuy@gmail.com>
2022-12-29 11:20:19 +0700

    revert sync state

--- src/app/store/actions/user/index.js
+++ src/app/store/actions/user/index.js
@@ -1,8 +1,6 @@
 export const SET_USER = 'SET_USER';
 export const REMOVE_USER = 'REMOVE_USER';
 export const SET_USER_PROCESSING_BY = 'SET_USER_PROCESSING_BY';
-export const IS_SYNCING_INQUIRY = 'IS_SYNCING_INQUIRY';
-export const IS_SYNCING_COMMENT = 'IS_SYNCING_COMMENT';
 
 export function setUser(state) {
   return {
@@ -20,20 +18,6 @@ export function removeUser() {
 export function userProcessingBy(state) {
   return {
     type: SET_USER_PROCESSING_BY,
-    state
-  };
-}
-
-export function isSyncingInquiry(state) {
-  return {
-    type: IS_SYNCING_INQUIRY,
-    state
-  };
-}
-
-export function isSyncingComment(state) {
-  return {
-    type: IS_SYNCING_COMMENT,
     state
   };
 }
