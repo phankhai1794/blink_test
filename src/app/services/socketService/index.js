3f4beb4ba27663e65c6ceed91720ea133d54d28f
Huy Nguyen <nguyenvuongnamhuy@gmail.com>
2022-12-29 03:51:29 +0000

    Revert "Merge branch 'hotfix/app-host' into 'develop'"
    This reverts merge request !693

--- src/app/services/socketService/index.js
+++ src/app/services/socketService/index.js
@@ -1,7 +1,8 @@
 import io from "socket.io-client";
 
 export const initiateSocketConnection = () => {
-  const hostSocket = window.location.origin; // Replace with localhost api when running in local environment
+  // const hostSocket = window.location.origin; // Replace with localhost api when running in local environment
+  const hostSocket = 'http://localhost:7000'; // Replace with localhost api when running in local environment
   console.log(`Connecting socket at: ${hostSocket}`);
   return io(hostSocket);
 };
