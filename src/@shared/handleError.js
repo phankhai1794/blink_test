820afd6eb4bd1d1e0752e9072b18639c5c236848
Huy Nguyen <nguyenvuongnamhuy@gmail.com>
2023-04-05 11:11:56 +0700

    update code fix authentication warning msg

--- src/@shared/handleError.js
+++ src/@shared/handleError.js
@@ -3,20 +3,21 @@ import * as Actions from 'app/store/actions';
 
 export const handleError = (dispatch, err) => {
   console.error(err);
+
   let status, message;
   if (err.response) {
     status = err.response.status || err.response.data?.error.status;
     message = err.response.data?.message || err.response.data?.error.message;
   }
   if (!message) message = err.message || "Your token has expired";
+  dispatch(Actions.showMessage({ message, variant: 'error' }));
+
   if ([401, 410].includes(status)) {
-    dispatch(Actions.showMessage({ message, variant: 'error' }));
     setTimeout(() => {
       dispatch(Actions.checkAuthToken(false));
       dispatch(Actions.checkAllow(false));
     }, 1000);
   }
-  else dispatch(Actions.showMessage({ message, variant: 'error' }));
   return [status, message];
 };
 
