73314077cb55571eb68313a259561fc94066c331
Huy Nguyen <nguyenvuongnamhuy@gmail.com>
2023-07-14 16:14:10 +0700

    hotfix null filling email

--- src/app/main/apps/OTPCheck.js
+++ src/app/main/apps/OTPCheck.js
@@ -339,7 +339,7 @@ const OtpCheck = ({ children }) => {
 
       // refill email when code expires
       let lastEmail = localStorage.getItem('lastEmail');
-      if (lastEmail && ["null", "undefined"].includes(lastEmail))
+      if (lastEmail && !["null", "undefined"].includes(lastEmail))
         setMail({ ...mail, value: lastEmail, isValid: isEmail(lastEmail) });
     }
 
