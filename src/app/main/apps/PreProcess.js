5030eb53a2841e1c39172caf9718dc498dec6ce5
vuong.le <vuong.le@cyberlogitec.com>
2023-08-22 03:15:27 +0000

    Merge branch 'CU-865cym5tj' into 'develop'
    Mod/CU-865cym5tj
    
    See merge request blink-workspace/portal!233

--- src/app/main/apps/PreProcess.js
+++ src/app/main/apps/PreProcess.js
@@ -5,39 +5,52 @@ import { useDispatch, useSelector } from 'react-redux';
 import { SocketContext } from 'app/AppContext';
 import { getPermissionByRole } from 'app/services/authService';
 import { getBlInfo } from 'app/services/myBLService';
-import * as AppAction from 'app/store/actions';
+import * as AppActions from 'app/store/actions';
 import { handleError } from '@shared/handleError';
 // import { categorizeInquiriesByUserType } from '@shared';
 
-import * as DraftActions from "./draft-bl/store/actions";
+import Deploying from '../pages/errors/deploying/Deploying';
+
+import * as DraftActions from './draft-bl/store/actions';
 import * as Actions from './workspace/store/actions';
 import * as FormActions from './workspace/store/actions/form';
 import * as InquiryActions from './workspace/store/actions/inquiry';
-import SubmitAnswerNotification from "./workspace/components/SubmitAnswerNotification";
+import SubmitAnswerNotification from './workspace/components/SubmitAnswerNotification';
 
 const PreProcess = ({ bl, children }) => {
   const dispatch = useDispatch();
   const socket = useContext(SocketContext);
 
+  const deploying = useSelector(({ fuse }) => fuse.message.deploying);
   const myBL = useSelector(({ workspace }) => workspace.inquiryReducer.myBL);
   const isLoading = useSelector(({ workspace }) => workspace.formReducer.isLoading);
-  const openNotification = useSelector(({ workspace }) => workspace.formReducer.openNotificationSubmitAnswer);
-  const openNotificationReply = useSelector(({ workspace }) => workspace.formReducer.openNotificationDeleteReply);
-  const openNotificationBLWarning = useSelector(({ workspace }) => workspace.formReducer.openNotificationBLWarning);
-  const openNotificationAmendment = useSelector(({ workspace }) => workspace.formReducer.openNotificationDeleteAmendment);
-  const openNotificationSubmitPreview = useSelector(({ workspace }) => workspace.formReducer.openNotificationSubmitPreview);
+  const openNotification = useSelector(
+    ({ workspace }) => workspace.formReducer.openNotificationSubmitAnswer
+  );
+  const openNotificationReply = useSelector(
+    ({ workspace }) => workspace.formReducer.openNotificationDeleteReply
+  );
+  const openNotificationBLWarning = useSelector(
+    ({ workspace }) => workspace.formReducer.openNotificationBLWarning
+  );
+  const openNotificationAmendment = useSelector(
+    ({ workspace }) => workspace.formReducer.openNotificationDeleteAmendment
+  );
+  const openNotificationSubmitPreview = useSelector(
+    ({ workspace }) => workspace.formReducer.openNotificationSubmitPreview
+  );
 
   const renderMsgNoti = () => {
-    if (openNotification)
-      return 'Your answer has been submitted successfully.';
-    else if (openNotificationReply)
-      return 'Your reply has been deleted.';
-    else if (openNotificationAmendment)
-      return 'Your amendment has been deleted.';
+    if (openNotification) return 'Your answer has been submitted successfully.';
+    else if (openNotificationReply) return 'Your reply has been deleted.';
+    else if (openNotificationAmendment) return 'Your amendment has been deleted.';
     else if (openNotificationBLWarning.status)
       return (
         <>
-          <img style={{ verticalAlign: 'middle', paddingBottom: 2, paddingLeft: 5, paddingRight: 5, }} src={`/assets/images/icons/warning.svg`} />
+          <img
+            style={{ verticalAlign: 'middle', paddingBottom: 2, paddingLeft: 5, paddingRight: 5 }}
+            src={`/assets/images/icons/warning.svg`}
+          />
           <span>{`The BL is opening by [${openNotificationBLWarning.userName}].`}</span>
         </>
       );
@@ -58,10 +71,15 @@ const PreProcess = ({ bl, children }) => {
   };
 
   const renderIconType = () => {
-    if (openNotification || openNotificationReply || openNotificationAmendment || openNotificationSubmitPreview)
+    if (
+      openNotification ||
+      openNotificationReply ||
+      openNotificationAmendment ||
+      openNotificationSubmitPreview
+    )
       return <img src={`/assets/images/icons/vector.svg`} />;
     return null;
-  }
+  };
 
   useEffect(() => {
     const bkgNo = window.location.pathname.split('/')[3];
@@ -69,7 +87,7 @@ const PreProcess = ({ bl, children }) => {
       dispatch(Actions.initBL(bkgNo));
     } else if (bl) {
       dispatch(FormActions.increaseLoading());
-      getBlInfo(bl).then(res => {
+      getBlInfo(bl).then((res) => {
         const { id, state, bkgNo } = res.myBL;
         dispatch(InquiryActions.setMyBL({ id, state, bkgNo }));
         dispatch(DraftActions.setBL({ id, state, bkgNo }));
@@ -87,20 +105,17 @@ const PreProcess = ({ bl, children }) => {
       const user = JSON.parse(localStorage.getItem('USER'));
 
       // user connect
-      const mybl = (user.userType === "ADMIN") ? [myBL.bkgNo, myBL.id] : [myBL.id, myBL.bkgNo];
-      socket.emit(
-        'user_connect',
-        {
-          mybl: mybl[0],
-          optSite: mybl[1], // opposite workspace (offshore or onshore/customer)
-          userName: user.displayName,
-          userType: user.userType
-        }
-      );
+      const mybl = user.userType === 'ADMIN' ? [myBL.bkgNo, myBL.id] : [myBL.id, myBL.bkgNo];
+      socket.emit('user_connect', {
+        mybl: mybl[0],
+        optSite: mybl[1], // opposite workspace (offshore or onshore/customer)
+        userName: user.displayName,
+        userType: user.userType
+      });
 
       socket.on('connect_error', (err) => {
         // if server has disconnected
-        history.push('/deploying')
+        dispatch(AppActions.warningDeploying(true));
       });
 
       // save socketId into window console after connecting
@@ -110,8 +125,7 @@ const PreProcess = ({ bl, children }) => {
 
       // receive a list of accessed onshores/customers
       socket.on('users_allowed_access', (data) => {
-        if (!data.includes(user.email))
-          window.location.reload();
+        if (!data.includes(user.email)) window.location.reload();
       });
 
       // receive a list users accessing
@@ -119,22 +133,36 @@ const PreProcess = ({ bl, children }) => {
         window.usersAccessing = usersAccessing;
 
         if (
-          user.userType === "ADMIN"
-          ||
-          (user.userType === "CUSTOMER" && myBL.state.includes("DRF_"))
+          user.userType === 'ADMIN' ||
+          (user.userType === 'CUSTOMER' && myBL.state.includes('DRF_'))
         ) {
-          const userLocal = localStorage.getItem('USER') ? JSON.parse(localStorage.getItem('USER')) : {};
+          const userLocal = localStorage.getItem('USER')
+            ? JSON.parse(localStorage.getItem('USER'))
+            : {};
           if (userLocal.displayName && usersAccessing.length) {
-            let permissions = await getPermissionByRole(userLocal.role).catch(err => handleError(dispatch, err));
-            if (userLocal.displayName === usersAccessing[0].userName) { // if to be the first user
+            let permissions = await getPermissionByRole(userLocal.role).catch((err) =>
+              handleError(dispatch, err)
+            );
+            if (userLocal.displayName === usersAccessing[0].userName) {
+              // if to be the first user
               dispatch(FormActions.toggleOpenBLWarning(false));
-            } else if (userLocal.displayName === usersAccessing[usersAccessing.length - 1].userName) { // if to be the last user
+            } else if (
+              userLocal.displayName === usersAccessing[usersAccessing.length - 1].userName
+            ) {
+              // if to be the last user
               const { pathname, search } = window.location;
               const rdrFrmWs = new URLSearchParams(search).get('rdrFrmWs');
               if (!rdrFrmWs)
-                dispatch(FormActions.toggleOpenBLWarning({ status: true, userName: usersAccessing[0].userName }));
+                dispatch(
+                  FormActions.toggleOpenBLWarning({
+                    status: true,
+                    userName: usersAccessing[0].userName
+                  })
+                );
               else {
-                permissions = await getPermissionByRole('Viewer').catch(err => handleError(dispatch, err));
+                permissions = await getPermissionByRole('Viewer').catch((err) =>
+                  handleError(dispatch, err)
+                );
                 setTimeout(() => {
                   const bl = new URLSearchParams(search).get('bl');
                   const url = new URL(window.location);
@@ -145,7 +173,7 @@ const PreProcess = ({ bl, children }) => {
             }
 
             setTimeout(() => {
-              dispatch(AppAction.setUser({ ...userLocal, permissions }));
+              dispatch(AppActions.setUser({ ...userLocal, permissions }));
             }, 500);
             sessionStorage.setItem('permissions', JSON.stringify(permissions));
           }
@@ -180,21 +208,27 @@ const PreProcess = ({ bl, children }) => {
 
   return (
     <>
-      {(isLoading <= 0) &&
-        <SubmitAnswerNotification
-          open={
-            openNotification ||
-            openNotificationReply ||
-            openNotificationAmendment ||
-            openNotificationBLWarning.status ||
-            openNotificationSubmitPreview
-          }
-          msg={renderMsgNoti()}
-          msg2={renderMsgNoti2()}
-          iconType={renderIconType()}
-        />
-      }
-      <>{children}</>
+      {deploying ? (
+        <Deploying />
+      ) : (
+        <>
+          {isLoading <= 0 && (
+            <SubmitAnswerNotification
+              open={
+                openNotification ||
+                openNotificationReply ||
+                openNotificationAmendment ||
+                openNotificationBLWarning.status ||
+                openNotificationSubmitPreview
+              }
+              msg={renderMsgNoti()}
+              msg2={renderMsgNoti2()}
+              iconType={renderIconType()}
+            />
+          )}
+          <>{children}</>
+        </>
+      )}
     </>
   );
 };
--- src/app/main/pages/pagesConfigs.js
+++ src/app/main/pages/pagesConfigs.js
@@ -11,7 +11,6 @@ import { ResetPassword2PageConfig } from './auth/reset-password-2/ResetPassword2
 import { ComingSoonPageConfig } from './coming-soon/ComingSoonPageConfig';
 import { Error404PageConfig } from './errors/404/Error404PageConfig';
 import { Error500PageConfig } from './errors/500/Error500PageConfig';
-import { DeployingConfig } from './errors/deploying/DeployingConfig';
 import { MaintenancePageConfig } from './maintenance/MaintenancePageConfig';
 import { ProfilePageConfig } from './profile/ProfilePageConfig';
 import { ClassicSearchPageConfig } from './search/classic/ClassicSearchPageConfig';
@@ -32,7 +31,6 @@ export const pagesConfigs = [
   ComingSoonPageConfig,
   Error404PageConfig,
   Error500PageConfig,
-  DeployingConfig,
   MaintenancePageConfig,
   ProfilePageConfig,
   ClassicSearchPageConfig,
