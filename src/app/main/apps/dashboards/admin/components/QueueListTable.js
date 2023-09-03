408202267bf72def3bdca71b5b0e6ecfc2b73971
phankhai <khai.phan@cyberlogitec.com>
2023-09-03 08:35:26 +0700

    onshore dashboard

--- src/app/main/apps/dashboards/admin/components/QueueListTable.js
+++ src/app/main/apps/dashboards/admin/components/QueueListTable.js
@@ -357,9 +357,11 @@ const Row = (props) => {
         <TableCell className={classes.cellBody}>
           {formatDate(row.lastUpdated, 'MMM DD YYYY HH:mm')}
         </TableCell>
-        <TableCell className={classes.cellBody}>
-          {formatDate(row.lastUpdatedAction, 'MMM DD YYYY HH:mm')}
-        </TableCell>
+        {userType !== 'ONSHORE' && (
+          <TableCell className={classes.cellBody}>
+            {formatDate(row.lastUpdatedAction, 'MMM DD YYYY HH:mm')}
+          </TableCell>
+        )}
         {columns.etd && (
           <TableCell className={classes.cellBody}>
             {row.etd && formatDate(row.etd, 'MMM DD YYYY HH:mm')}
@@ -688,11 +690,10 @@ const QueueListTable = () => {
             .split(',')
             .filter((bkg) => bkg)
             .map((bkg) => bkg.trim().toUpperCase()),
-          blinkStatus: searchQueueQuery.blStatus,
           countries,
           office,
           isMe: searchQueueQuery.isMe,
-          status: searchQueueQuery.status,
+          status: searchQueueQuery.blStatus,
         },
         sort: searchQueueQuery.sortField
       })
@@ -817,11 +818,13 @@ const QueueListTable = () => {
                       />
                     </div>
                   </TableCell>
-                  <TableCell className={classes.cellHead}>
-                    <div className={clsx(classes.lineMinWidth, classes.lineColumn)}>
-                      <span>Latest Action Update</span>
-                    </div>
-                  </TableCell>
+                  {userType !== 'ONSHORE' && (
+                    <TableCell className={classes.cellHead}>
+                      <div className={clsx(classes.lineMinWidth, classes.lineColumn)}>
+                        <span>Latest Action Update</span>
+                      </div>
+                    </TableCell>
+                  )}
                   {columns.etd && (
                     <TableCell className={classes.cellHead}>
                       <div className={clsx(classes.lineMinWidth, classes.lineColumn)}>
@@ -857,14 +860,14 @@ const QueueListTable = () => {
                       </div>
                     </TableCell>
                   )}
-                  {columns.customerS && userType !== 'ONSHORE' && (
+                  {columns.customerS && (
                     <TableCell className={classes.cellHead}>
                       <div className={clsx(classes.lineMinWidth, classes.lineColumn)}>
                         <span>Customer Status</span>
                       </div>
                     </TableCell>
                   )}
-                  {columns.onshoreS && userType !== 'ONSHORE' && (
+                  {columns.onshoreS && (
                     <TableCell className={classes.cellHead}>
                       <div className={clsx(classes.lineMinWidth, classes.lineColumn)}>
                         <span>Onshore Status</span>
