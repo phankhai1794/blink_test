2127f73d2d071298ae61cab2d64653d4dd372ff7
minhnt98 <minhnt28101998@gmail.com>
2023-09-26 16:06:24 +0700

    update

--- src/app/main/apps/workspace/components/UserInfo.js
+++ src/app/main/apps/workspace/components/UserInfo.js
@@ -3,6 +3,8 @@ import { Avatar } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import { cyan } from '@material-ui/core/colors';
 import Popover from '@material-ui/core/Popover';
+import IconButton from '@material-ui/core/IconButton';
+import Icon from '@material-ui/core/Icon';
 
 const useStyles = makeStyles((theme) => ({
   name: {
@@ -25,18 +27,24 @@ const useStyles = makeStyles((theme) => ({
   },
   styleMark: {
     color: 'gray'
+  },
+  mailPaper: {
+    minWidth: 300,
+    padding: 5
   }
 }));
 
-const MailInfo = ({ emails }) => {
-  const [anchorEl, setAnchorEl] = useState(false);
+const MailInfo = (props) => {
+  const { anchorEl, setAnchorEl, emails } = props;
+
+  const classes = useStyles();
 
   const closeEmailList = () => {
     setAnchorEl(null);
   };
   return (
     <Popover
-      // classes={{ paper: classes.mailList }}
+      classes={{ paper: classes.mailPaper }}
       open={Boolean(anchorEl)}
       anchorEl={anchorEl}
       onClose={closeEmailList}
@@ -48,18 +56,27 @@ const MailInfo = ({ emails }) => {
         vertical: 'top',
         horizontal: 'center'
       }}>
-      <div>
-        <span>from</span>
+      <div style={{ display: 'flex', margin: '10px 16px', gap: '20px' }}>
+        <span style={{ width: 30 }}>from:</span>
+        <span>Offshore</span>
       </div>
       {[
-        { label: 'To', value: emails.to },
-        { label: 'Cc', value: emails.cc },
-        { label: 'Bcc', value: emails.bcc }
+        { label: 'to:', value: ['minhntvuo@gmail.com'] || emails.to },
+        {
+          label: 'cc:',
+          value: ['abc@gmail.com', 'abc1@@gmail.com', 'abc2@gmail.com'] || emails.cc
+        },
+        { label: 'bcc:', value: [] || emails.bcc }
       ].map(({ label, value }, index) => (
         <>
           {value.length ? (
-            <div>
-              <span>{label}</span>
+            <div style={{ display: 'flex', margin: '10px 16px', gap: '20px' }}>
+              <span style={{ width: 30 }}>{label}</span>
+              <div style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
+                {value.map((v) => (
+                  <span>{v}</span>
+                ))}
+              </div>
             </div>
           ) : null}
         </>
@@ -71,9 +88,13 @@ const MailInfo = ({ emails }) => {
 const UserInfo = (props) => {
   const { name, time, avatar, state, status, emails } = props;
   const classes = useStyles();
-
+  const [anchorEl, setAnchorEl] = useState(false);
+  const showMailInfo = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
   return (
     <div className="flex">
+      <MailInfo anchorEl={anchorEl} setAnchorEl={setAnchorEl} />
       {avatar ? (
         <Avatar src={avatar} />
       ) : (
@@ -99,7 +120,12 @@ const UserInfo = (props) => {
                 status === 'UPDATE' && <span className={classes.styleMark}> - Edited</span>}
               {(['REP_DRF_DELETED', 'REP_SENT_DELETED'].includes(state) ||
                 status === 'DELETED') && <span className={classes.styleMark}> - Deleted</span>}
-              {['INQ_SENT'].includes(state) && `| to ${emails}`}
+              <>
+                {['INQ_SENT', 'REP_Q_SENT'].includes(state) && ` | to ${emails}`}
+                <IconButton style={{ padding: 1 }} onClick={showMailInfo}>
+                  <Icon size="small">expand_more</Icon>
+                </IconButton>
+              </>
             </p>
           </div>
         </div>
