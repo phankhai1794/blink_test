835743250b47ee8c7036fb6101d16fdc3334ed64
vuongle <vuong.le@cyberlogitec.com>
2023-09-28 13:46:21 +0700

    disable upload OPUS

--- src/app/main/apps/workspace/components/InquiryViewer.js
+++ src/app/main/apps/workspace/components/InquiryViewer.js
@@ -357,6 +357,11 @@ const InquiryViewer = (props) => {
     RD_TERMS,
     BL_TYPE,
     T_VVD,
+    SHIPPER,
+    CONSIGNEE,
+    FORWARDER,
+    ALSO_NOTIFY,
+    NOTIFY
   ];
 
   const isDisableBtnUpload = () => {
